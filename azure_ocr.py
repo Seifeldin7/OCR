aa59ccdf3db57ecadf1b91da0e6d929f0e7378ac
Seif Eldin Mohamed Hussein <31385107+Seifeldin7@users.noreply.github.com>
2019-09-18 03:20:18 +0400

    Update azure_ocr.py

--- azure_ocr.py
+++ azure_ocr.py
@@ -1,28 +1,25 @@
 import requests
 import time
+from PIL import Image
+from io import BytesIO
+vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
 
-ocr_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/ocr"
-image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/" + \
-    "Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png"
+ocr_url = vision_base_url + "ocr"
+#image_url = "https://i.stack.imgur.com/vrkIj.png"
 
 
-# headers = {'Ocp-Apim-Subscription-Key': "12bb5c83fb9a4d5e9df7417fd0016200"}
 params = {'language': 'unk', 'detectOrientation': 'false'}
-data = {'url': image_url}
+#data = {'url': image_url}
 
-image_path = "images/car_wash.png"
-with open(image_path, 'rb') as f:
-    img_data = f.read()
-print(img_data)
+image_path = "img2.jpg"
+image_data = open(image_path, "rb").read()
 # Set Content-Type to octet-stream
 headers = {'Ocp-Apim-Subscription-Key': "12bb5c83fb9a4d5e9df7417fd0016200", 'Content-Type': 'application/octet-stream'}
 # put the byte array into your post request
-# response = requests.post(ocr_url, headers=headers, params=params, data = image_data)
-
+print(image_data)
 start = time.time()
-response = requests.post(ocr_url, headers=headers, params=params, json=data)
+response = requests.post(ocr_url, headers=headers, params=params, data=image_data)
 # response.raise_for_status()
 analysis = response.json()
 print("time is ", time.time() - start)
-print(analysis)
-print("image url is ",image_url)
+#print("image url is ",image_url)
