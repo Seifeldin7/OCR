4c1c44ff17b93a21c6ca410d6311b8d42df3c9c0
mgtm98 <m.g.t.m.prog@gmail.com>
2019-09-17 21:28:51 +0200

    show image in it's original ratio

--- east_detection.py
+++ east_detection.py
@@ -7,8 +7,13 @@ from decode import decode
 import numpy as np
 from imutils.object_detection import non_max_suppression
 
-img = cv2.imread("images/sign.jpg",1)               
-img = cv2.resize(img, (160,160))
+orig = cv2.imread("images/lebron_james.jpg",1)  
+(H, W) = orig.shape[:2] 
+new_H = 320
+new_W = 320
+rW = W / float(new_W)
+rH = H / float(new_H)            
+img = cv2.resize(orig, (new_W,new_H))
 
 #get NN layer names
 layerNames = ["feature_fusion/Conv_7/Sigmoid","feature_fusion/concat_3"]
@@ -16,16 +21,20 @@ layerNames = ["feature_fusion/Conv_7/Sigmoid","feature_fusion/concat_3"]
 net = cv2.dnn.readNet("frozen_east_text_detection.pb")
 #get blobs from img 
 #TODO What is blobs
-blob = cv2.dnn.blobFromImage(img, 1.0, (160, 160),(123.68, 116.78, 103.94), swapRB=True, crop=False)
+blob = cv2.dnn.blobFromImage(img, 1.0, (new_W, new_H),(123.68, 116.78, 103.94), swapRB=True, crop=False)
 net.setInput(blob)
 #NN feedforward
 (scores, geometry) = net.forward(layerNames)
 
-confidences , rects = decode(scores, geometry, 0.5)
+confidences , rects = decode(scores, geometry, 0.8)
 boxes = non_max_suppression(np.array(rects), probs=confidences)
 
 for (startX, startY, endX, endY) in boxes:
-	cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    startX = int(startX * rW)
+    startY = int(startY * rH)
+    endX = int(endX * rW)
+    endY = int(endY * rH)
+    cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
-cv2.imshow("image", img)
+cv2.imshow("image", orig)
 cv2.waitKey(0)
