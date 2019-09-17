6c65e1c3cd202497df7543c90bebf8fa086ba722
mgtm98 <m.g.t.m.prog@gmail.com>
2019-09-17 19:33:22 +0200

    added images file added NN data file added base script to detect Text "

--- east_detection.py
+++ east_detection.py
@@ -3,13 +3,29 @@ import sys
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
 import cv2
+from decode import decode
+import numpy as np
+from imutils.object_detection import non_max_suppression
 
-orignal = cv2.imread("images/sign.jpg",1)
-img = cv2.resize(orignal, (320,320))
+img = cv2.imread("images/sign.jpg",1)               
+img = cv2.resize(img, (160,160))
 
+#get NN layer names
+layerNames = ["feature_fusion/Conv_7/Sigmoid","feature_fusion/concat_3"]
+#load NN
+net = cv2.dnn.readNet("frozen_east_text_detection.pb")
+#get blobs from img 
+#TODO What is blobs
+blob = cv2.dnn.blobFromImage(img, 1.0, (160, 160),(123.68, 116.78, 103.94), swapRB=True, crop=False)
+net.setInput(blob)
+#NN feedforward
+(scores, geometry) = net.forward(layerNames)
+
+confidences , rects = decode(scores, geometry, 0.5)
+boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+for (startX, startY, endX, endY) in boxes:
+	cv2.rectangle(img, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
 cv2.imshow("image", img)
-cv2.imshow("orignal image", orignal)
-
-
 cv2.waitKey(0)
