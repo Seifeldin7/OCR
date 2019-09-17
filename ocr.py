3eee98707b29a7683398dd732936160884f910d6
seif eldin Mohamed <s.seif26@yahoo.com>
2019-09-17 17:09:24 +0400

    recognizes well written text only

---
+++ ocr.py
@@ -0,0 +1,134 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+
+# Path of working folder on Disk
+HSV_RANGES = {
+    # red is a major color
+    'red': [
+        {
+            'lower': np.array([0, 39, 64]),
+            'upper': np.array([20, 255, 255])
+        },
+        {
+            'lower': np.array([161, 39, 64]),
+            'upper': np.array([180, 255, 255])
+        }
+    ],
+    # yellow is a minor color
+    'yellow': [
+        {
+            'lower': np.array([21, 39, 64]),
+            'upper': np.array([40, 255, 255])
+        }
+    ],
+    # green is a major color
+    'green': [
+        {
+            'lower': np.array([41, 39, 64]),
+            'upper': np.array([80, 255, 255])
+        }
+    ],
+    # cyan is a minor color
+    'cyan': [
+        {
+            'lower': np.array([81, 39, 64]),
+            'upper': np.array([100, 255, 255])
+        }
+    ],
+    # blue is a major color
+    'blue': [
+        {
+            'lower': np.array([101, 39, 64]),
+            'upper': np.array([140, 255, 255])
+        }
+    ],
+    # violet is a minor color
+    'violet': [
+        {
+            'lower': np.array([141, 39, 64]),
+            'upper': np.array([160, 255, 255])
+        }
+    ],
+    # next are the monochrome ranges
+    # black is all H & S values, but only the lower 25% of V
+    'black': [
+        {
+            'lower': np.array([0, 0, 0]),
+            'upper': np.array([180, 255, 63])
+        }
+    ],
+    # gray is all H values, lower 15% of S, & between 26-89% of V
+    'gray': [
+        {
+            'lower': np.array([0, 0, 64]),
+            'upper': np.array([180, 38, 228])
+        }
+    ],
+    # white is all H values, lower 15% of S, & upper 10% of V
+    'white': [
+        {
+            'lower': np.array([0, 0, 229]),
+            'upper': np.array([180, 38, 255])
+        }
+    ]
+}
+
+# Creates a binary mask from HSV image using given colors.
+def create_mask(hsv_img, colors):
+
+
+
+    mask = np.zeros((hsv_img.shape[0], hsv_img.shape[1]), dtype=np.uint8)
+
+    for color in colors:
+        for color_range in HSV_RANGES[color]:
+            mask += cv2.inRange(
+                hsv_img,
+                color_range['lower'],
+                color_range['upper']
+            )
+
+    return mask
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+
+    img = cv2.cvtColor(img_path, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite("removed_noise.png", img)
+
+    #  Apply threshold to get image with only black and white
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 2)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite("thres.png", img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(img)
+
+    return result
+
+
+print(get_string("add-text.jpg"))
+
+# i tried to get the words of each color
+
+
+#img = cv2.imread('add-text.jpg')
+#img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#y_mask = create_mask(img_hsv, ['yellow'])
+#mask_img = cv2.bitwise_and(img_hsv, img_hsv, mask=y_mask)
+#print(get_string(mask_img))
+#cv2.imshow("image",mask_img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
